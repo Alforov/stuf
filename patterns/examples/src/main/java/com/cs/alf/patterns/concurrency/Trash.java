fd4eea19ebc0802519a3413df0ba4e875ce5b0db
Alforov <mmmmisha@mail.ru>
2017-04-22 01:14:50 +0300

    todo jms

--- patterns/examples/src/main/java/com/cs/alf/patterns/concurrency/Trash.java
+++ patterns/examples/src/main/java/com/cs/alf/patterns/concurrency/Trash.java
@@ -1,19 +1,39 @@
 package com.cs.alf.patterns.concurrency;
 
-import java.util.concurrent.TimeUnit;
+
+import java.util.*;
 
 /**
  * Created by mikhail_alferov on 07.04.2017.
  */
 public class Trash {
     public static void main(String[] args) {
-        System.out.println(Integer.MAX_VALUE);
-        Integer integer = Integer.valueOf("26001306255838");
-        try {
-            TimeUnit.MINUTES.sleep(1);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+     String target = "hello buy hello buy remote !!";
+        String[] split = target.split(" ");
+        Map<String, Integer> map = new HashMap<>();
+        for (String s : split) {
+            if(map.containsKey(s)){
+                map.put(s, map.get(s)+ 1);
+            }else {
+                map.put(s,1);
+            }
         }
-        System.out.println("the end");
+        Integer t =null;
+        Integer c = 4;
+        if(t>c){
+            System.out.println("------------------");
+        }
+        ArrayList<Map.Entry<String,Integer>> entries = new ArrayList<>();
+        entries.addAll(map.entrySet());
+        Collections.sort(entries, (o1, o2) -> {
+            if(o1.getValue() == null || o1.getValue()<o2.getValue()) return -1;
+            if(o1.getValue().equals(o2.getValue())) return 1;
+            return 0;
+        });
+        for (Map.Entry<String, Integer> entry : entries) {
+            System.out.printf("count %s value: %s\n" ,entry.getValue(),entry.getKey());
+        }
+
+
     }
 }
