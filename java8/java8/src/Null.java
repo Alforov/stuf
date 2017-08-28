6b2d587f30593ea17d78d2f8bd21f823d2e49342
mikhail_alferov <mikhail_alferov@csltd.com.ua>
2017-08-28 09:53:06 +0300

    spring lifecycle

---
+++ java8/java8/src/Null.java
@@ -0,0 +1,8 @@
+/**
+ * Created by mikhail_alferov on 22.08.2017.
+ */
+public class Null {
+    public static void main(String[] args) {
+        String s = "ae20faf4-397c-493e-9b45-023cd2735ad9";
+    }
+}
