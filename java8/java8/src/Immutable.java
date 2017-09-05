717d99cebb6b819192874f45402a8217034bffd5
mikhail_alferov <mikhail_alferov@csltd.com.ua>
2017-09-05 10:57:11 +0300

    work 20170905

--- java8/java8/src/Immutable.java
+++ java8/java8/src/Immutable.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
  * Created by mikhail_alferov on 01.09.2017.
  */
 public class Immutable {
+    private static  volatile String S = "D";
     public static void main(String[] args) {
         BigDecimal one = BigDecimal.ONE;
 
