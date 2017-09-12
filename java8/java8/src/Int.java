07a153f071b7a2100e68a1c345ed083571cd9d01
mikhail_alferov <mikhail_alferov@csltd.com.ua>
2017-09-12 14:56:12 +0300

    work20170912

---
+++ java8/java8/src/Int.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by mikhail_alferov on 11.09.2017.
+ */
+public class Int {
+
+    public static void main(String[] args) {
+        String s = null;
+        System.out.println(new StringBuilder().append(s).toString());
+    }
+}
